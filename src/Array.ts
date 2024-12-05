88761c2cfb04907e06df5e6368c10f3a26f703e1
MelvinAnthony <23cp0310003@student.hindustanuniv.ac.in>
2024-12-05 15:04:23 +0530

    Array Concepts

---
+++ src/Array.ts
@@ -0,0 +1,34 @@
+// TypeScript Arrays
+
+
+// Implicit
+/* let numSample = [1,2,3,4,5]
+let strSample = ['a','b','c','d']
+let boolSample = [true, false]
+
+ // numSample.push('d') // in ts not work but js will work.
+
+console.log(numSample) */
+
+// Explicit
+
+/* let numSample:number[] = [1,2,3,4,5]
+let strSample:string[] = ['a','b','c','d']
+let boolSample:boolean[] = [true, false] */
+
+
+// Declare and assign the value
+
+/* let numSample:number[] =[];
+let strSample:string[] = [];
+let boolSample:boolean[] = [];
+
+numSample.push(1) */
+
+
+
+// Read only 
+
+let numSample: readonly number[] =[1,2,3,4,5];
+
+//numSample.push(4)  // we cannont add new values
