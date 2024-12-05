7926ecbe93a2f3a394396a67f1736ecd62cf4f5a
MelvinAnthony <23cp0310003@student.hindustanuniv.ac.in>
2024-12-05 14:52:58 +0530

    Data Types

---
+++ dest/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// Primitive Types
+// number, bool and string
+let empName = 'test';
+let employeeNamr = 'testing';
+let isPermanent = true;
+let age = 30;
+let mul = (num) => {
+    return 10 * num;
+};
+console.log(mul(10));
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+// Primitive Types
+// number, bool and string
+
+let empName = 'test';
+
+let employeeNamr: string = 'testing';
+
+let isPermanent: boolean = true
+
+let age: number = 30
+
+
+
+let mul = (num:number) =>{
+    return 10 * num
+}
+
+console.log(mul(10))
+
